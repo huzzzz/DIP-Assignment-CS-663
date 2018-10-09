c8324065a3fa5cc32ada4c1db89e4efcc90c2400
aniket <aniket8897@gmail.com>
2018-10-09 19:37:18 +0530

    Minor edit in 4

--- A4/assignment4_SVD_FaceRecognition/assignment4_SVD_FaceRecognition/4/code/MySVD.m
+++ A4/assignment4_SVD_FaceRecognition/assignment4_SVD_FaceRecognition/4/code/MySVD.m
@@ -1,21 +1,22 @@
 function [U,S,V] = MySVD(A)
+    
+    [m, n] = size(A);
 
 	% eigs gives eigenvalues in sorted order
 
 	% get left singular vectors of A. 
 	% AA' = (USV') (VS'U') = US(VV')S'U' = USS'U' = USSU'. Hence U is eigenvector matrix of AA'
-	[U,~] = eigs( A * A' );
+	[U,~] = eigs( A * A', m );
 
 	% get right singular vectors of A
 	% A'A = (VS'U') (USV') = VS'(U'U)SV' = VS'SV' = VS'SV'. Hence V is eigenvector matrix of A'A
-	[V,~] = eigs( A' * A );
+	[V,~] = eigs( A' * A, n );
 
 	% Since U and V are orthornormal, U' * (U*S*V') * V = S 
 	S = U' * A * V;
 
 	% But we need only non-negative values on diagonal for S
 	% So we adjust S and U to maintain consistency. Negating does not affect the orthonormality of U
-    [m,n] = size(S);
     for d=1:min(m,n)
     	if(S(d,d)<0)
     		U(:,d) = -U(:,d);
